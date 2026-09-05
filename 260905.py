4a831e15f9e0d86d3afb0522494e34a007d2c356
taeyoung <yys2131@naver.com>
2026-09-05 16:05:57 +0900

    day_1

---
+++ 260905.py
@@ -0,0 +1,19 @@
+# 프린터
+# 파이썬은 들여쓰기의 영향을 받기 때문에 무조건 앞에를 띄우지 않는다
+# print("It's my first code using pythone")
+#
+#
+# #변수확인
+# name = "kimtaeyoung"
+# print(name)
+#
+# #입력받는법 input
+# newName = input("what your name")
+#
+# print(newName)
+#
+# #길이확인법 len
+# length=len(newName)
+# print(length)
+
+#test1
